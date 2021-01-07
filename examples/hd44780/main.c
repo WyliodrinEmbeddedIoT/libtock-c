c32891f0cb19c42e9c72ecdb3deebaae83dc9902
Teona Severin <teona.severin9@gmail.com>
2021-01-07 15:30:18 +0200

    updated userspace application

--- examples/hd44780/main.c
+++ examples/hd44780/main.c
@@ -11,15 +11,15 @@
 
 int main(void) {
   int ret = hd44780_init(15);
-  printf("%d\n", ret);
+  ret = hd44780_display();
   if (ret == TOCK_SUCCESS) {
     ret = hd44780_set_cursor(0, 0);
-    ret = hd44780_print_string("ollaaa!");
+    ret = hd44780_print_string("Hello!");
 
     delay_ms(5000);
 
     ret = hd44780_set_cursor(0, 1);
-    ret = hd44780_print_string("Buenas noches!");
+    ret = hd44780_print_string("Goodbyee!");
 
     delay_ms(2000);
     ret = hd44780_clear();
