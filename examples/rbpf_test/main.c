2824f0af9cea646fcacc742cd2d93ba625d75ffd
Vochescu Alexandru <alexvochescu@gmail.com>
2021-05-26 18:21:43 +0300

    Adding rbpf test example

---
+++ examples/rbpf_test/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <tock.h>
+#include <timer.h>
+
+#define DRIVER_NUM_LATENCY 0x10001
+
+int main(void) 
+{
+	printf("Inceput de main");
+	syscall_return_t res = command(DRIVER_NUM_LATENCY, 0, 0, 0);
+  if (res.type == TOCK_SYSCALL_SUCCESS) {
+  	printf("Success!!");
+  } else {
+  	printf("Fails.. :( !!");
+    return tock_error_to_rcode (res.data[0]);
+  }
+  // printf("command_command: %d us; command_subscribe: %d us\r\n", command_command, command_subscribe);
+  return 0;
+}
