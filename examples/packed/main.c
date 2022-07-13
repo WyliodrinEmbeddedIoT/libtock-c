96ab8de4a3fd95524b487ace8f5f8d3de37a1a1c
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2022-07-13 16:53:22 +0300

    running syscalls

--- examples/packed/main.c
+++ examples/packed/main.c
@@ -1,7 +1,18 @@
 #include <led.h>
 #include <timer.h>
+#include <gpio.h>
+
+// use packed system calls
+#define PACKED
+
+// gpio driver 4, led driver 2
+// when using the gpio driver, make sure no to change the GPIO_PIN
+#define DRIVER 2
+
+#define SYSCALLS_NUMBER 100
 
 #if defined(__thumb__)
+#define GPIO_PIN 16
 static syscall_return_t packed(uint32_t count, uint32_t *data,
                          bool stop_error) {
   register uint32_t r0 __asm__ ("r0") = count;
@@ -22,6 +33,7 @@ static syscall_return_t packed(uint32_t count, uint32_t *data,
   return rval;
 }
 #elif defined(__riscv)
+#define GPIO_PIN 0
 static syscall_return_t packed(uint32_t count, uint32_t *data,
                          bool stop_error) {
   register uint32_t a0  __asm__ ("a0") = count;
@@ -45,35 +57,78 @@ static syscall_return_t packed(uint32_t count, uint32_t *data,
 
 int main(void) {
 
-    uint32_t data[25*5];
+  #ifdef PACKED
+  uint32_t data[SYSCALLS_NUMBER*5];
+  // printf("Packed syscalls, driver: %d, syscalls number: %d\n", DRIVER, SYSCALLS_NUMBER);
+  #endif
 
-    bool light = true;
+  bool light = true;
 
-    while (true) {
+  gpio_enable_output(GPIO_PIN);
 
-    for (int i=0; i < 25; i++) {
+  while (true) {
+
+    command(4, 2, GPIO_PIN, 0);
+
+    #ifdef PACKED
+    for (int i=0; i < SYSCALLS_NUMBER; i++) {
       data[0 + 5*i] = 2;
-      data[1 + 5*i] = 4;
+      data[1 + 5*i] = DRIVER;
       data[2 + 5*i] = (light?2:3);
-      data[3 + 5*i] = i;
+      data[3 + 5*i] = 2;
       data[4 + 5*i] = 0;
     }
-    syscall_return_t res = packed(25, data, true);
+    syscall_return_t res = packed(SYSCALLS_NUMBER, data, true);
+    #endif
 
-    int executed_syscalls = 25;
+    // RISC-V Gpio 25 152 us
+    // RISC-V Gpio 50 296 us
+    // RISC-V Gpio 100 580 us
+    // RISC-V Led 25 136us
+    // RISC-V Led 50 256us
+    // RISC-V Led 100 408us - 520us
+    // ARM Gpio 25 690us
+    // ARM Gpio 50 1.340ms
+    // ARM Gpio 100 2.64ms
+    // ARM Led 25 720us
+    // ARM Led 50 1.440ms
+    // ARM Led 100 2.800ms
 
-    if (res.type == TOCK_SYSCALL_FAILURE_U32) {
-      printf ("Executed %ld syscalls\n", 25 - res.data[0]); 
-      executed_syscalls = 25 - res.data[0];
+    #ifndef PACKED
+    for (int i=0; i < SYSCALLS_NUMBER; i++) {
+      command (DRIVER, (light?2:3), 2, 0);
     }
+    #endif
+
+    // RISC-V Gpio 25 188us
+    // RISC-V Gpio 50 360us
+    // RISC-V Gpio 100 736us
+    // RISC-V Led 25 170us
+    // RISC-V Led 50 336us
+    // RISC-V Led 100 660us
+    // ARM Gpio 25 740us
+    // ARM Gpio 50 1.46ms
+    // ARM Gpio 100 2.96ms
+    // ARM Led 25 760us
+    // ARM Led 50 1.520ms
+    // ARM Led 100 3.040ms
+
+    command(4, 3, GPIO_PIN, 0);
+
+    // int executed_syscalls = 25;
+
+    // if (res.type == TOCK_SYSCALL_FAILURE_U32) {
+    //   printf ("Executed %ld syscalls\n", 25 - res.data[0]); 
+    //   executed_syscalls = 25 - res.data[0];
+    // }
 
     light = !light;
 
-    for (int i = 0; i < executed_syscalls; i++) {
-      printf ("syscall %d res %ld\n", i, data[1 + 5*i]);
-    }
+    // for (int i = 0; i < executed_syscalls; i++) {
+    //   printf ("syscall %d res %ld\n", i, data[1 + 5*i]);
+    // }
 
     // This delay uses an underlying timer in the kernel.
-    delay_ms(250);
+    delay_ms(5);
   }
 }
